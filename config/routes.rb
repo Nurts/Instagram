7dfc4524b4516e02f248dbc988e2fede7a27e9ab
Nurts <inurtas1999@gmail.com>
2018-06-17 18:23:03 +0600

    First commit:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/contact', to: "pages#contact", via: :get
+  get '/contact', to: "pages#contact"
   get '/about', to: "pages#about"
   root "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
